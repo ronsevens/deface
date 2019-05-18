5830407ab3c39aac17ff37d49db6b9eaab87d944
Mr.R0N1Z <50039352+Mr-R0N1Z@users.noreply.github.com>
2019-05-18 22:30:12 +0700

    Add files via upload

---
+++ deface_v1.sh
@@ -0,0 +1,139 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo $i Downloading...
+sleep 3
+echo $i 2Pack Is Downloading....
+sleep 4
+clear
+figlet 10%
+sleep 0.1
+clear
+figlet 9%
+sleep 0.1
+clear
+figlet 8%
+sleep 0.1
+clear
+figlet 7%
+sleep 0.1
+clear
+figlet 6%
+sleep 0.1
+clear
+figlet 5%
+sleep 0.1
+clear
+figlet 4%
+sleep 0.1
+clear
+figlet 3%
+sleep 0.1
+clear
+figlet 2%
+sleep 0.1
+clear
+figlet 1%
+sleep 0.1
+clear
+figlet 10%
+sleep 0.1
+clear
+figlet 9%
+sleep 0.1
+clear
+figlet 8%
+sleep 0.1
+clear
+figlet 7%
+sleep 0.1
+clear
+figlet 6%
+sleep 0.1
+clear
+figlet 5%
+sleep 0.1
+clear
+figlet 4%
+sleep 0.1
+clear
+figlet 3%
+sleep 0.1
+clear
+figlet 2%
+sleep 0.1
+clear
+figlet 1%
+sleep 0.1
+clear
+figlet 10%
+sleep 0.1
+clear
+figlet 9%
+sleep 0.1
+clear
+figlet 8%
+sleep 0.1
+clear
+figlet 7%
+sleep 0.1
+clear
+figlet 6%
+sleep 0.1
+clear
+figlet 5%
+sleep 0.1
+clear
+figlet 4%
+sleep 0.1
+clear
+figlet 3%
+sleep 0.1
+clear
+figlet 2%
+sleep 0.1
+clear
+figlet 1%
+sleep 0.1
+clear
+echo $ku Downloading Succes
+sleep 4
+clear
+echo $pur jangan sekali² recode atau kena akibatnya
+sleep 4
+clear
+echo $i R0N1Z telah membuat Script ini, lama codingnya cuk -_-, hargailah karya seseorang
+sleep 4
+clear
+echo $i test
+sleep 0.1
+clear
+figlet Deface
+sleep 1
+echo $me Tunggu 5detik
+sleep 5
+clear
+echo $ku tulis nick kamu: #lihat
+read nick #look
+clear
+
+roll
+echo $i Hai $nick selamat datang!
+sleep 3
+clear
+echo $i @r0n1z: Hi $nick Welcome
+sleep 2
+clear
+git clone https://github.com/Mr-R0N1Z/deface
+sleep 0.1
+cd deface
+sleep 0.1
+sh bum.sh
+
